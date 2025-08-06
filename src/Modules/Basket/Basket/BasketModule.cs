33c825a3cda8f2ef147d8577352206c5db035914
MD SAIDUJJAMAN SAJIB <saidujjaman.sajib@valmetpartners.com>
2025-08-06 18:40:19 +0600

    Project structure created.

---
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Basket;
+
+public static class BasketModule
+{
+    public static IServiceCollection AddBasketModule(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
+}
+
---
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace Catalog;
+
+public static class CatalogModule
+{
+    public static IServiceCollection AddCatalogModule(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
+}
