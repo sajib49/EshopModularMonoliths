360c51e15a84acfaafcf5748b15ae102fe39562f
MD SAIDUJJAMAN SAJIB <saidujjaman.sajib@valmetpartners.com>
2025-08-13 16:28:55 +0600

    Docker compose has been added.

---
+++ src/Modules/Catalog/Catalog/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Shared.DDD;
+global using Catalog.Products.Models;
+global using Microsoft.EntityFrameworkCore;
+global using System.Reflection;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,50 @@
+﻿using Catalog.Products.Events;
+
+namespace Catalog.Products.Models;
+
+public class Product : Aggregate<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public List<string> Category { get; private set; } = new();
+    public string Description { get; private set; } = default!;
+    public string ImageFile  { get; private set; } = default!;
+    public decimal Price { get; private set; }
+
+    public static Product Create(Guid id, string name, List<string> category, string description, string imageFile, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        
+        var product =  new Product { 
+            Category = category, 
+            Name = name, 
+            Description = description, 
+            ImageFile = imageFile, 
+            Price = price 
+        };
+
+        product.AddDomainEvent(new ProductCreatedEvent(product));
+
+        return product;
+    }
+
+   public void Update(Guid id, string name, List<string> category, string description, string imageFile, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        Category = category;
+        Name = name;
+        Description = description;
+        ImageFile = imageFile;
+        Price = price;
+
+        if(Price != price)
+        {
+            Price = price;
+            AddDomainEvent(new ProductPriceChangesEvent(this));
+        }
+    }
+
+}
