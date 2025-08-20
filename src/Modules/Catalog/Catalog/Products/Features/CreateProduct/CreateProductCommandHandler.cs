b4f75f42a065d95b02a7cd4c8c6a27a7637f007e
MD SAIDUJJAMAN SAJIB <saidujjaman.sajib@valmetpartners.com>
2025-08-20 18:23:40 +0600

    Added generic command and query handler in shared module.

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Catalog.Products.Features.CreateProduct;
+
+public record CreateProductCommand
+    (string name, List<string> category, string description, string imageFile, decimal price)
+    : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid id);
+
+public class CreateProductCommandHandler
+    : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
